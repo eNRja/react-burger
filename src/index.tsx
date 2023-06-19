a3bd4cdc797a29e6adb79f2818262afacaf6cc45
Алексей <satukov90@yandex.ru>
2023-06-19 22:20:22 +0300

    some fix

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,17 @@ import App from './components/app/app';
 import { Provider } from 'react-redux';
 import { createRoot } from 'react-dom/client';
 import store from './services/store'
-import { BrowserRouter} from 'react-router-dom';
+import { HashRouter} from 'react-router-dom';
 
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
